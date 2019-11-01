9f562f61df9c322678a296de32fe2a6371c7a657
Isuru Weerasinghe <donudayanga99@gmail.com>
2019-11-01 12:54:40 +0530

    ui updated

--- frontEnd/src/app/components/home/home.component.ts
+++ frontEnd/src/app/components/home/home.component.ts
@@ -7,6 +7,8 @@ import { stringify } from 'querystring';
 import { SearchService } from '../../shared/search.service';
 import { HttpClient } from '@angular/common/http';
 import { CloudData, CloudOptions } from 'angular-tag-cloud-module';
+import { Label, Color } from 'ng2-charts';
+import { ChartsModule } from 'ng2-charts';
 
 
 @Component({
@@ -19,7 +21,7 @@ export class HomeComponent implements OnInit {
   //--------------------------------------------WORD CLOUD----------------------------------------------------
   options: CloudOptions = {
     // if width is between 0 and 1 it will be set to the size of the upper element multiplied by the value
-    width: 500,
+    width: 550,
     height: 378,
     overflow: false,
   };
@@ -30,7 +32,31 @@ export class HomeComponent implements OnInit {
     {text: 'ක්‍රිකට්', weight: 25},
     {text: 'ක්රිකට් ', weight: 14, color: '#21618C'},
     {text: 'කුසල් මෙන්ඩිස්', weight: 12, color: '#2874A6'},
-    {text: 'දිනේෂ් චන්දිමාල්', weight: 11, color: '#2E86C1'}
+    {text: 'දිනේෂ් චන්දිමාල්', weight: 11, color: '#2E86C1'},
+    {text: 'විකට්', weight: 6, color: '#2E86C1'},
+    {text: 'එක්දින', weight: 13, color: '#2E86C1'},
+    {text: 'නවසීලන්ත', weight: 9, color: '#2E86C1'},
+    {text: 'ක්‍රිකට් ', weight: 5},
+    {text: 'ක්රිකට් ', weight: 10, color: '#2E86C1'},
+    {text: 'කුසල් මෙන්ඩිස්', weight: 12, color: '#2874A6'},
+    {text: 'දිනේෂ් චන්දිමාල්', weight: 11, color: '#2E86C1'},
+    {text: 'දිමුත් කරුණාරත්න', weight: 16, color: '#3498DB'},
+    {text: 'මැතිව්ස් ', weight: 8, color: '#5DADE2'},
+    {text: 'ලංකාව', weight: 10, color: '#85C1E9'},
+    {text: 'ශතක ', weight: 6, color: '#AED6F1'},
+    {text: 'නිරෝෂන් දික්වැල්ල', weight: 10, color: '#2E86C1'},
+    {text: 'සනත් ජයසූරිය', weight: 10, color: '#1B4F72'},
+    {text: 'ටෙස්ට්', weight: 9, color: '#2E86C1'},
+    {text: 'තරග', weight: 10, color: '#1B4F72'},
+    {text: 'පැරදිල', weight: 10, color: '#21618C'},
+    {text: 'ක්‍රිඩා ඇමති', weight: 10, color: '#2874A6'},
+    {text: 'තිරිමාන්න', weight: 15, color: '#2E86C1'},
+    {text: 'දිනුව', weight: 10, color: '#3498DB'},
+    {text: 'කණ්ඩායමකම', weight: 8, color: '#5DADE2'},
+    {text: 'දේශපාලනයට', weight: 9, color: '#85C1E9'},
+    {text: 'ශතක ', weight: 12, color: '#AED6F1'},
+    {text: 'ලෝක කුසලාන', weight: 13, color: '#2E86C1'},
+    {text: 'තරගාවලිය', weight: 11, color: '#1B4F72'}
   ];
 
 
@@ -43,6 +69,8 @@ export class HomeComponent implements OnInit {
   public doughnutChartLabels = ['Negative','Positive'];
   public doughnutChartData = [50,50];
   public doughnutChartType = 'doughnut';
+  //public donutColors = { backgroundColor: ['rgba(77,83,96,1)','rgba(0,0,96,1)'] };
+  public doughnutChartColors: any[] = [ {backgroundColor: ['red','green']} ]
 
   videos: any;
   videosid: any;
